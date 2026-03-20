fdf9d480b85efe4a18645296b84a23a354476002
timzhangqy-sketch <timzhangqy@gmail.com>
2026-03-20 18:28:20 +0800

    refactor: ContextPanel replace MA5/MA10 with 5d/10d returns

--- src/components/ContextPanel/sections/StockContextQuote.tsx
+++ src/components/ContextPanel/sections/StockContextQuote.tsx
@@ -34,22 +34,19 @@ export default function StockContextQuote({ data, loading }: Props) {
       <Row label="成交额(亿)" value={fmt(data.amountYi)} groupStart />
       <Row label="换手率%" value={fmt(data.turnoverRate)} />
       <Row label="VR量比" value={data.vr != null ? `${fmt(data.vr)}x` : '--'} />
-      {/* Group 3: MA */}
-      <Row label="MA5" value={fmt(data.ma5)} groupStart />
-      <Row label="MA10" value={fmt(data.ma10)} />
+      {/* Group 3: Trend + MA */}
+      <Row label="5日涨幅" value={fmtRatio(data.pctChg5d)} cls={pctCls(data.pctChg5d)} groupStart />
+      <Row label="10日涨幅" value={fmtRatio(data.pctChg10d)} cls={pctCls(data.pctChg10d)} />
       <Row label="MA20" value={fmt(data.ma20)} />
       <Row label="距MA20" value={fmtRatio(data.closeVsMa20Pct)} cls={pctCls(data.closeVsMa20Pct)} />
       {/* Group 4: Valuation */}
       <Row label="PE(TTM)" value={fmt(data.peTtm, 1)} groupStart />
       <Row label="PB" value={fmt(data.pb)} />
       <Row label="市值(亿)" value={fmt(data.totalMvYi, 1)} />
-      {/* Group 5: Industry */}
+      {/* Group 5: Industry + 20d */}
       <Row label="行业" value={data.industry ?? '--'} groupStart />
-      {/* Group 6: Trend */}
-      <Row label="5日涨幅" value={fmtRatio(data.pctChg5d)} cls={pctCls(data.pctChg5d)} groupStart />
-      <Row label="10日涨幅" value={fmtRatio(data.pctChg10d)} cls={pctCls(data.pctChg10d)} />
       <Row label="20日涨幅" value={fmtRatio(data.pctChg20d)} cls={pctCls(data.pctChg20d)} />
-      {/* Group 7: Range */}
+      {/* Group 6: Range */}
       {data.high60d != null && (
         <Row label="60日区间" value={`${fmt(data.low60d)} ~ ${fmt(data.high60d)}`} groupStart />
       )}
